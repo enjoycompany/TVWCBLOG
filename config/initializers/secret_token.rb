b7c00766d4691b70ff5ed992ceef08ad5e64141a
enjoycompany <enjoycompany@gmail.com>
2013-06-15 15:58:19 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tvwcblog::Application.config.secret_token = 'eab730b8891c3c4ab0af9506929e897a55fbdeb102865eb264cb1a5af4272a739b8b4f6f03127e139050af1764af4c68f8ab62ce5a13adfa50610902d9e6bde7'
